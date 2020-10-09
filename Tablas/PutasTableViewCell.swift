55782af38fe2b738bdc1df1721767b61ee257e94
Joseba Uri <joseba_urizarbarrena_apps1ta1920@cev.com>
2020-10-09 18:05:25 +0200

    Commit tablas

---
+++ Tablas/PutasTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PutasTableViewCell.swift
+//  Tablas
+//
+//  Created by Apps2t on 09/10/2020.
+//  Copyright © 2020 Apps2t. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class PutasTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet var textView: UITextView!
+
+    
+}
--- Tablas/ViewController.swift
+++ Tablas/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var datos: [String] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datos.count
+    } //filas
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaID", for: indexPath)
+        celda.textLabel?.text = datos [indexPath.row] //Array de String
+        return celda
+    } //como pintar cada fila
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    } //seleccion de una fila
+    
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
 
